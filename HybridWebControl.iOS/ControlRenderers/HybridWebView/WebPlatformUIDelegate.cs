8db34e3b9e6d51880106acd58b036f8e52917265
Kolesnyk Sviatoslav <sviatoslav.k@sbtech.com>
2017-04-27 13:23:23 +0300

    #1: + JavaScript alerts on iOS handling within WebView was implemented. The code was mostly reused from 8a9de23e6. + Release version increased due to nuget.org upload.

--- HybridWebControl.iOS/ControlRenderers/HybridWebView/WebPlatformUIDelegate.cs
+++ HybridWebControl.iOS/ControlRenderers/HybridWebView/WebPlatformUIDelegate.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using WebKit;
 using UIKit;
 
@@ -20,5 +21,61 @@ namespace HybridWebPlatform.iOS
 
             return new WKWebView(new CoreGraphics.CGRect(0, 0, 0, 0), configuration);
         }
+
+#region Handling JavaScript alerts
+        public override void RunJavaScriptAlertPanel(WKWebView webView, string message, WKFrameInfo frame, Action completionHandler)
+        {
+            var alertController = UIAlertController.Create("", message, UIAlertControllerStyle.Alert);
+
+            var actionOk = UIAlertAction.Create("Ok", UIAlertActionStyle.Default, (UIAlertAction obj) =>
+            {
+                completionHandler();
+            });
+
+            alertController.AddAction(actionOk);
+
+            UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(alertController, true, null);
+        }
+
+        public override void RunJavaScriptConfirmPanel(WKWebView webView, string message, WKFrameInfo frame, Action<bool> completionHandler)
+        {
+            var alertController = UIAlertController.Create("", message, UIAlertControllerStyle.Alert);
+
+            var actionOk = UIAlertAction.Create("Ok", UIAlertActionStyle.Default, (UIAlertAction obj) =>
+            {
+                completionHandler(true);
+            });
+            var actionCancel = UIAlertAction.Create("Cancel", UIAlertActionStyle.Default, (UIAlertAction obj) =>
+             {
+                 completionHandler(false);
+             });
+
+            alertController.AddAction(actionOk);
+            alertController.AddAction(actionCancel);
+
+            UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(alertController, true, null);
+        }
+
+        public override void RunJavaScriptTextInputPanel(WKWebView webView, string prompt, string defaultText, WKFrameInfo frame, Action<string> completionHandler)
+        {
+            var alertController = UIAlertController.Create("", prompt, UIAlertControllerStyle.Alert);
+
+            alertController.AddTextField((UITextField obj) => obj.Text = defaultText);
+            var actionOk = UIAlertAction.Create("Ok", UIAlertActionStyle.Default, (UIAlertAction obj) =>
+            {
+                string text = alertController.TextFields.Any() ? alertController.TextFields.First().Text : defaultText;
+                completionHandler(text);
+            });
+            var actionCancel = UIAlertAction.Create("Cancel", UIAlertActionStyle.Default, (UIAlertAction obj) =>
+             {
+                 completionHandler(null);
+             });
+
+            alertController.AddAction(actionOk);
+            alertController.AddAction(actionCancel);
+
+            UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(alertController, true, null);
+        }
+#endregion
     }
 }
