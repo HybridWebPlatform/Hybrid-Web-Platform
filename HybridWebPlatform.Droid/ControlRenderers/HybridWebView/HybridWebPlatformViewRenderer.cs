61d6ef498b0bcc913a7170c1368cdd98c0aeb9c1
Kolesnyk Sviatoslav <sviatoslav.k@sbtech.com>
2017-04-27 14:16:08 +0300

    Files allocated in correct folders

--- HybridWebPlatform.Droid/ControlRenderers/HybridWebView/HybridWebPlatformViewRenderer.cs
+++ HybridWebPlatform.Droid/ControlRenderers/HybridWebView/HybridWebPlatformViewRenderer.cs
@@ -137,6 +137,7 @@ namespace HybridWebPlatform.Droid
 				webView.Settings.SetGeolocationEnabled(true);
 				webView.Settings.SetSupportMultipleWindows(true);
 				webView.Settings.SetSupportZoom(false);
+				webView.Settings.UserAgentString = "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19";
 
 				webView.AddJavascriptInterface(new HybridWebPlatformXamarinApi(this), "Xamarin");
 
